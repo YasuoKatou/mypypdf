750aea0a55820f60a812b2ee6a7e19df05c95762
YasuoKatou <yasuokatou@gmail.com>
2022-12-18 17:03:13 +0900

    文字データの読込み

--- pdf_objects/contents.py
+++ pdf_objects/contents.py
@@ -2,6 +2,38 @@ import re
 from .pdf_reader import PDFReader
 from .cross_reference import PDFCrossReferenceTable
 
+class CharInfo:
+    def __init__(self, m):
+        self._x = float(m.group('X'))
+        self._y = float(m.group('Y'))
+        self._code = int(m.group('CODE'), 16)
+        print('x:{}, y:{}, code:{:04x}'.format(self._x, self._y, self._code))
+
+class StringBlock:
+    _font_name = None
+    _tm = None
+    _chars = []
+    _RE_TEXT_TF = re.compile(r'/(?P<FONT_NAME>[\w\d]+)\s+\d+\.*\d*\s+Tf')
+    _RE_TEXT_TM = re.compile(r'(?P<A>[\-\d\.]+)\s+(?P<B>[\-\d\.]+)\s+(?P<C>[\-\d\.]+)\s+(?P<D>[\-\d\.]+)\s+(?P<E>[\-\d\.]+)\s+(?P<F>[\-\d\.]+)\s+Tm')
+    _RE_TEXT_TD_TJ = re.compile(r'(?P<X>[\-\d\.]+)\s+(?P<Y>[\-\d\.]+)\s+Td\s+<(?P<CODE>[0-9A-Fa-f]+)>\s+Tj')
+    def __init__(self, b):
+        #print('++++++++++++++')
+        #print(b)
+        m = self._RE_TEXT_TF.search(b)
+        if m:
+            self._font_name = m.group('FONT_NAME')
+            print(self._font_name)
+        m = self._RE_TEXT_TM.search(b)
+        if m:
+            self._tm = (float(m.group('A')), float(m.group('B')), float(m.group('C')),
+                        float(m.group('D')), float(m.group('E')), float(m.group('F')))
+            print('a:{}, b:{}, c:{}, d:{}, e:{}, f:{}'.format(
+                self._tm[0], self._tm[1], self._tm[2],
+                self._tm[3], self._tm[4], self._tm[5]
+            ))
+        for m in self._RE_TEXT_TD_TJ.finditer(b):
+            self._chars.append(CharInfo(m))
+
 class PDFContents:
     _reader = PDFReader()
     _xref = PDFCrossReferenceTable()
@@ -22,18 +54,11 @@ class PDFContents:
         s = self._reader.read_object(d.getOffset(), ignore_newline=False)
         self._getString(d.getOffset(), s)
 
+    _str_block = []
     _RE_TEXT_BLOCK = re.compile(r'^BT\n(?P<BODY>.*?)^ET\n?', flags=re.MULTILINE | re.DOTALL)
-    _RE_TEXT_TF = re.compile(r'/(?P<FONT_NAME>[\w\d]+)\s+\d+\.*\d*\s+Tf', flags=re.MULTILINE)
-    _RE_TEXT_TM = re.compile(r'')
-    _RE_TEXT_TJ = re.compile(r'')
     def _getString(self, offset, source):
         #print('--------------')
         #print(source)
         for m in self._RE_TEXT_BLOCK.finditer(source):
-            b = m.group('BODY')
-            #print('++++++++++++++')
-            #print(b)
-            mf = self._RE_TEXT_TF.match(b)
-            if mf:
-                print(mf.group('FONT_NAME'))
+            self._str_block.append(StringBlock(m.group('BODY')))
 #[EOF]
